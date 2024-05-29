f09a9a54a786459046560cbe09e4d7f4494a3b34
ArdakHere <mrglok1@gmail.com>
2024-05-29 13:51:22 +0500

    3343

--- airway_ext_krisha/content.js
+++ airway_ext_krisha/content.js
@@ -1,22 +1,3 @@
-// content.js
 
-function extractCoordinates() {
-    // Adjust the selectors based on the actual HTML structure of the listing page
-    const latElement = document.querySelector('meta[property="lat"]');
-    const lonElement = document.querySelector('meta[property="lon"]');
-    
-    
-    const lat = latElement ? latElement.getAttribute('content') : null;
-    const lon = lonElement ? lonElement.getAttribute('content') : null;
-  
-    return { lat, lon };
-  }
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === "getCoordinates") {
-      const lat = "43.443"; // Example lat, replace with actual extraction logic
-      const lon = "75.223"; // Example lon, replace with actual extraction logic
-      sendResponse({ text: { lat: lat, lon: lon } });
-    }
-  });
   
   
